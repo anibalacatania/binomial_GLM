95770fc9ed4aefc9b468104d519fdff757000288
Aníbal Alejandro Catania <anibalacatania@gmail.com>
2024-03-29 21:39:03 -0300

    va

--- glm.R
+++ glm.R
@@ -2,7 +2,8 @@ library(lme4)
 library(readxl)
 library(car)
 library(emmeans)
-data <- read_excel("harpo.xlsx")
+library(ggplot2)
+data <- read_excel("dataok.xlsx")
 data$Usuario<-as.factor(data$Usuario)
 data$parrafo<-as.factor(data$parrafo)
 data$modelo<-as.factor(data$modelo)
@@ -17,3 +18,14 @@ summary(M1)
 Anova(M1, type=3)
 lsm<-emmeans(M1, pairwise~ modelo, type="response")
 confint(lsm)
+plot(lsm)
+
+a<-ranef(M1)
+a$Usuario$`(Intercept)`
+a$parrafo$`(Intercept)`
+library(sjPlot)
+library(sjlabelled)
+library(sjmisc)
+c<-plot_model(M1, type="re", show.values = TRUE, value.offset = .3)
+c[[2]]
+
