452cb80a94609fe2d9ce1ea2c3121363ae6920b8
Aníbal Alejandro Catania <anibalacatania@gmail.com>
2024-03-29 21:03:14 -0300

    glm

--- glm.R
+++ glm.R
@@ -1,11 +1,19 @@
 library(lme4)
 library(readxl)
 library(car)
+library(emmeans)
 data <- read_excel("harpo.xlsx")
 data$Usuario<-as.factor(data$Usuario)
 data$parrafo<-as.factor(data$parrafo)
 data$modelo<-as.factor(data$modelo)
 str(data)
-M1 <- glmer(preferecnia ~  modelo* (1 |parrafo) *(1 |Usuario) , data = data, family = binomial)
-M1
+M1 <- glmer(preferecnia ~  modelo+
+             (1 |parrafo)+
+              (1 |Usuario)+
+             (1 |parrafo:modelo)+
+              (1 |parrafo:Usuario)+
+              (1 |Usuario:modelo), data = data, family = binomial)
+summary(M1)
 Anova(M1, type=3)
+lsm<-emmeans(M1, pairwise~ modelo, type="response")
+confint(lsm)
